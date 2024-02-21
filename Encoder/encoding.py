7e30bbac86861046d3b5536d212296c1ddc39198
Artemii <angap4@gmail.com>
2024-02-21 23:25:54 +1000

    encoder rdy

--- Encoder/encoding.py
+++ Encoder/encoding.py
@@ -3,9 +3,44 @@
 """
 __all__ = ['encode']
 
-DEFAULT_KEYWORD = "Depression"
-SYM_INDEX = 65
+DEFAULT_KEYWORD = "Депрессия"
+ALPHABET = {'А': 0, 'Б': 1, 'В': 2, 'Г': 3,
+            'Д': 4, 'Е': 5, 'Ё': 6, 'Ж': 7,
+            'З': 8, 'И': 9, 'Й': 10, 'К': 11,
+            'Л': 12, 'М': 13, 'Н': 14, 'О': 15,
+            'П': 16, 'Р': 17, 'С': 18, 'Т': 19,
+            'У': 20, 'Ф': 21, 'Х': 22, 'Ц': 23,
+            'Ч': 24, 'Ш': 25, 'Щ': 26, 'Ъ': 27,
+            'Ы': 28, 'Ь': 29, 'Э': 30, 'Ю': 31,
+            'Я': 32,
+            'а': 33, 'б': 34, 'в': 35, 'г': 36,
+            'д': 37, 'е': 38, 'ё': 39, 'ж': 40,
+            'з': 41, 'и': 42, 'й': 43, 'к': 44,
+            'л': 45, 'м': 46, 'н': 47, 'о': 48,
+            'п': 49, 'р': 50, 'с': 51, 'т': 52,
+            'у': 53, 'ф': 54, 'х': 55, 'ц': 56,
+            'ч': 57, 'ш': 58, 'щ': 59, 'ъ': 60,
+            'ы': 61, 'ь': 62, 'э': 63, 'ю': 64,
+            'я': 65,
+            '.': 66, ',': 67, '!': 68, '?':69, ' ': 70}
 
-def encode(keyword=DEFAULT_KEYWORD):
-    for sym in keyword:
-        print(ord(sym) - SYM_INDEX)
+REV_ALPHABET = {v: k for k, v in ALPHABET.items()}
+SYM_INDEX = len(ALPHABET)
+
+
+def encode(message: str, keyword: str = DEFAULT_KEYWORD) -> str:
+    result = ""
+    counter = 0
+    for sym in message:
+        if sym == '\n':
+            result += '\n'
+            continue
+        key_sym = keyword[counter]
+
+        diff = ALPHABET[sym] - ALPHABET[key_sym]
+        diff = (diff + SYM_INDEX) % SYM_INDEX
+        result += REV_ALPHABET[diff]
+
+        counter= (counter + 1) % len(keyword)
+
+    return result
